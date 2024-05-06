a785bbc94cc8e319ebf6f94f14e2f261088f6e6c
Nickkiemaf <mikailajibola1424@gmail.com>
2024-05-06 12:52:58 -0700

    configuration managment

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,19 @@
+#!/usr/bin/puppet
+
+# Puppet manifest to install Flask version 2.1.0 and Werkzeug version 2.1.1 using pip3
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+package { 'python3-pip':
+  ensure => installed,
+}
